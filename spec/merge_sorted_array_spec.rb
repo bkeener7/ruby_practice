19d97d68584dffa3a4f31d7f2327d90875be7b62
bkeener <bkeener7@gmail.com>
2023-05-11 11:38:17 -0600

    Add additional test

--- spec/merge_sorted_array_spec.rb
+++ spec/merge_sorted_array_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe MergeSortedArray do
     n = 3
     expect(merge_sorted_array.merge(nums1, m, nums2, n)).to eq([1, 2, 2, 3, 5, 6])
   end
+
+  it 'can merge an array with an empty one' do
+    nums1 = [1]
+    m = 1
+    nums2 = []
+    n = 0
+    expect(merge_sorted_array.merge(nums1, m, nums2, n)).to eq([1])
+  end
 end
