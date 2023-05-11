74c22e9cf0e70c4b16e48ccce7bb7d7556350cc5
bkeener <bkeener7@gmail.com>
2023-05-11 11:36:55 -0600

    Add ability to merge arrays

--- lib/merge_sorted_array.rb
+++ lib/merge_sorted_array.rb
@@ -1,2 +1,7 @@
 class MergeSortedArray
+  def merge(nums1, m, nums2, n)
+    nums1 = nums1.take(m)
+    nums2 = nums2.take(n)
+    nums1 = (nums1 + nums2).sort
+  end
 end
--- spec/merge_sorted_array_spec.rb
+++ spec/merge_sorted_array_spec.rb
@@ -1,11 +1,15 @@
 RSpec.describe MergeSortedArray do
-  let(:merge) { MergeSortedArray.new }
-  let(:nums1) { [1, 2, 3, 0, 0, 0] }
-  let(:m1) { 3 }
-  let(:nums2) { [2, 5, 6] }
-  let(:n1) { 3 }
+  let(:merge_sorted_array) { MergeSortedArray.new }
 
   it 'exists' do
-    expect(merge).to be_an_instance_of(MergeSortedArray)
+    expect(merge_sorted_array).to be_an_instance_of(MergeSortedArray)
+  end
+
+  it 'can merge arrays in non-decreasing order' do
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    expect(merge_sorted_array.merge(nums1, m, nums2, n)).to eq([1, 2, 2, 3, 5, 6])
   end
 end
