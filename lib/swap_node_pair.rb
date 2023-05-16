07df95703f47cea623d7b8be2aea9dcbc4d4d714
bkeener <bkeener7@gmail.com>
2023-05-16 11:26:26 -0600

    Finish tests and class for swap_node_pair

--- lib/swap_node_pair.rb
+++ lib/swap_node_pair.rb
@@ -1,3 +1,20 @@
 class SwapNodePair
-  def swap_pairs(head); end
+  attr_accessor :val, :next
+
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+
+  def swap_pairs(head)
+    return head if head.nil? || head.next.nil?
+
+    first_node = head
+    second_node = head.next
+
+    first_node.next = swap_pairs(second_node.next)
+    second_node.next = first_node
+
+    second_node
+  end
 end
--- spec/swap_node_pair_spec.rb
+++ spec/swap_node_pair_spec.rb
@@ -1,12 +1,32 @@
 RSpec.describe SwapNodePair do
-  let(:swap_node_pair) { SwapNodePair.new }
+  it 'swaps two adjacent nodes in a linked list' do
+    node4 = SwapNodePair.new(4)
+    node3 = SwapNodePair.new(3, node4)
+    node2 = SwapNodePair.new(2, node3)
+    head = SwapNodePair.new(1, node2)
 
-  it 'exists' do
-    expect(swap_node_pair).to be_an_instance_of(SwapNodePair)
+    swapped_head = SwapNodePair.new.swap_pairs(head)
+
+    expect(swapped_head.val).to eq(2)
+    expect(swapped_head.next.val).to eq(1)
+    expect(swapped_head.next.next.val).to eq(4)
+    expect(swapped_head.next.next.next.val).to eq(3)
+    expect(swapped_head.next.next.next.next).to be_nil
   end
 
-  xit 'swap two adjacent nodes in a linked list' do
-    head = [1, 2, 3, 4]
-    expect(swap_node_pair.swap_pairs(head)).to eq([2, 1, 4, 3])
+  it 'returns an empty list when input is empty' do
+    head = nil
+
+    swapped_head = SwapNodePair.new.swap_pairs(head)
+
+    expect(swapped_head).to eq(nil)
+  end
+
+  it 'returns unchanged list when there are no pairs to swap' do
+    head = SwapNodePair.new(1)
+
+    swapped_head = SwapNodePair.new.swap_pairs(head)
+
+    expect(swapped_head.val).to eq(1)
   end
 end
